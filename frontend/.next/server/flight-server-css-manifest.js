bd55f3264a9b539cb89bba60bd2e0a407b487a92
bad fad <badfad@edu.novia.fi>
2023-11-11 16:38:38 +0200

    added cors

--- frontend/.next/server/flight-server-css-manifest.js
+++ frontend/.next/server/flight-server-css-manifest.js
@@ -1,4 +1,12 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {},
-  "cssModules": {}
+  "cssImports": {
+    "C:\\Users\\bador\\Desktop\\junction-cure-ai\\frontend\\app\\layout.tsx": [
+      "C:\\Users\\bador\\Desktop\\junction-cure-ai\\frontend\\styles\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\bador\\Desktop\\junction-cure-ai\\frontend\\app\\page": [
+      "C:\\Users\\bador\\Desktop\\junction-cure-ai\\frontend\\styles\\globals.css"
+    ]
+  }
 }
