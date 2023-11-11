eef7865678aac72b697e52e4f63aa3509ef45b7b
junezhuo <yzzhuo2@gmail.com>
2023-11-11 10:41:09 +0200

    fix: frontend page

--- frontend/pages/demo.tsx
+++ frontend/pages/demo.tsx
@@ -2,7 +2,51 @@ import { AnimatePresence, motion } from "framer-motion";
 import { RadioGroup } from "@headlessui/react";
 import Link from "next/link";
 import { useRef, useState, useEffect, useCallback } from "react";
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
+function MyDisclosure() {
+      return (
+        <div className="w-full px-4 pt-16">
+          <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                    <span>How do you feel like today? Tell me your symptoms today?</span>
+                    <ChevronUpIcon
+                      className={`${
+                        open ? 'rotate-180 transform' : ''
+                      } h-5 w-5 text-purple-500`}
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                   
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+            <Disclosure as="div" className="mt-2">
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                    <span>What is your ?</span>
+                    <ChevronUpIcon
+                      className={`${
+                        open ? 'rotate-180 transform' : ''
+                      } h-5 w-5 text-purple-500`}
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                    No.
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
+        </div>
+      )
+}
 const questions = [
   {
     id: 1,
@@ -27,11 +71,7 @@ export default function DemoPage() {
     <AnimatePresence>
         <div className="w-full min-h-screen flex flex-col px-4 pt-2 pb-8 md:px-8 md:py-2 bg-[#FCFCFC] relative overflow-x-hidden">
           <div className="w-full flex flex-col max-w-[1080px] mx-auto mt-[10vh] overflow-y-auto pb-8 md:pb-12">
-              <div className="flex flex-row items-center space-x-1">
-                <p className="text-[14px] font-normal leading-[20px] text-[#1a2b3b]">
-                  Demo Pages
-                </p>
-              </div>
+             <MyDisclosure />
           </div>
         </div>
     </AnimatePresence>
