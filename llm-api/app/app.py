517412f356cf54602941de5295a0958dd4176e18
mallasto <anton.mallasto@gmail.com>
2023-11-11 13:47:08 +0200

    Move assets to directory

--- llm-api/app/app.py
+++ llm-api/app/app.py
@@ -11,9 +11,9 @@ import llm
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger()
 
-RUBRIC = json.load(open("rubric.json"))
-SYMPTOMS = json.load(open('symptoms.json'))
-DISORDERS = json.load(open('disorders.json'))
+RUBRIC = json.load(open("assets/rubric.json"))
+SYMPTOMS = json.load(open('assets/symptoms.json'))
+DISORDERS = json.load(open('assets/disorders.json'))
 
 def handler(event, context=None):
     request = utils.get_request(event)
