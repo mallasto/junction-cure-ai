8cb73acc42bc9497cca4db74bbbee0b816b8cedc
mallasto <anton.mallasto@gmail.com>
2023-11-11 13:44:50 +0200

    Update test

--- llm-api/app/test-app.py
+++ llm-api/app/test-app.py
@@ -3,11 +3,10 @@ import json
 
 journals = json.load(open('../../data/journals.json'))
 request = {
-    'api_name': 'user_feedback',
     'entries': journals[0]['entries']
 }
 
 result = app.handler(request)
 
-with open(f"./result-{request['api_name']}.json", 'w') as f:
+with open(f"./result.json", 'w') as f:
     json.dump(result, f, indent=2)
